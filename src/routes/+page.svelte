edc8d55f172782ca549640e9eaa0ab9c5a801a13
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 21:03:31 +0200

    Fetch & display list of posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,41 @@
-<main class="min-h-screen bg-slate-900">
-    <h1>Welcome to SvelteKit</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    
-    <button class="btn btn-primary">Hello daisyUI</button>
-</main>
-    
+<script lang="ts">
+    import * as config from '$lib/config';
+    import { formatDate } from '$lib/utils';
+    import { ArrowRight, CalendarDays } from 'lucide-svelte';
+
+    export let data;
+</script>
+
+<svelte:head>
+    <title>{config.title}</title>
+</svelte:head>
+
+<article>
+    <section>
+        <div class="hero h-96 bg-base-200">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold">Welcome to my blog</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              </div>
+            </div>
+        </div>
+    </section>
+    <ul class="flex flex-col gap-8">
+        {#each data.posts as post}
+            <li class="flex flex-col gap-2 border-2 bg-base-100 border-base-300 rounded-xl p-12 shadow-xl">
+                <h2 class="text-4xl text-primary">{post.title}</h2>
+                <p class="flex gap-2 text-sm text-secondary items-center">
+                    <CalendarDays size="16" />
+                    {formatDate(post.date)}
+                </p>
+                <p class="text-base">{post.description}</p>
+                <span class="self-end">
+                    <a href={post.slug} class="btn btn-primary btn-sm">
+                        Read more <ArrowRight size="18" />
+                    </a>
+                </span>
+            </li>
+        {/each}
+    </ul>
+</article>
