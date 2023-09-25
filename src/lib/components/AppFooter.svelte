bc886371a13b87c1cad062b7dda8945582ea9651
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 14:00:52 +0200

    Add footer

---
+++ src/lib/components/AppFooter.svelte
@@ -0,0 +1,8 @@
+<footer class="footer p-10 bg-base-300 footer-center text-base-content">
+    <p class="footer-center block">
+        Blog made using SvelteKit through tutorial by
+        <a href="https://www.youtube.com/watch?v=RhScu3uqGd0" class="pl-1 link-primary">
+            Joy of Code
+        </a>
+    </p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
-<script>
+<script lang="ts">
     import '../app.css';
+    import AppFooter from '$lib/components/AppFooter.svelte';
 </script>
 
+<main>
 <slot />
+    <AppFooter />
+</main>
