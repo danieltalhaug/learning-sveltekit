95a649e39ce1a6df7e69b6824fdca33d6067b135
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 23:03:35 +0200

    Create transition component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,18 @@
     import AppHeader from '$lib/components/AppHeader.svelte';
     import AppFooter from '$lib/components/AppFooter.svelte';
     import Container from '$lib/components/Container.svelte';
+    import Transition from '$lib/components/Transition.svelte';
+
+    export let data;
 </script>
 
 <main>
     <AppHeader />
     <main class="min-h-screen bg-base-200">
         <Container>
-            <slot />
+            <Transition url={data.url}>
+                <slot />
+            </Transition>
         </Container>
     </main>
     <AppFooter />
