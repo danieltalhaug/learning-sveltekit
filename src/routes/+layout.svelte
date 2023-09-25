5e6c8063443bbb4c791ea5cdc3ecabef674a290d
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 21:02:51 +0200

    Wrap content in container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,15 @@
     import '../app.css';
     import AppHeader from '$lib/components/AppHeader.svelte';
     import AppFooter from '$lib/components/AppFooter.svelte';
+    import Container from '$lib/components/Container.svelte';
 </script>
 
 <main>
     <AppHeader />
-    <slot />
+    <main class="min-h-screen bg-base-200">
+        <Container>
+            <slot />
+        </Container>
+    </main>
     <AppFooter />
 </main>
