23bfc9b78c2ad08934a14550c11e29254cd3291c
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 22:16:42 +0200

    Setup for code highlighting with shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import shiki from 'shiki';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +14,14 @@ const config = {
 		/** @type {import('mdsvex').MdsvexOptions} */
 		mdsvex({
 			extensions: ['.md'],
+			highlight: {
+				highlighter: async (code, lang = 'text') => {
+					const highlighter = await shiki.getHighlighter({theme: 'poimandres',});
+					const html = highlighter.codeToHtml(code, { lang });
+
+					return `{@html \`${escapeSvelte(html)}\`}`;
+				}
+			}
 		})
 	],
 
