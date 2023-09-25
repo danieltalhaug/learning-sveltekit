85f2c702be9d8fa02f66ef3383c579876c42766b
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-25 14:24:31 +0200

    Install & setup mdsvex for parsing markdown

---
+++ src/lib/posts/first-post.md
@@ -0,0 +1,14 @@
+---
+title: First post
+description: Shabladong
+date: '2023'
+---
+
+# Hello ding dong
+Testing yo, sup
+
+## Baddabing yo
+
+```ts
+const greet = (name: string): string => 'Hello' + name;
+```
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		// https://mdsvex.pngwn.io/docs/ For Mdsvex docs!
+		/** @type {import('mdsvex').MdsvexOptions} */
+		mdsvex({
+			extensions: ['.md'],
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
