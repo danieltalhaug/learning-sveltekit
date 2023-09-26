e512d803dff04c1d7f4b6237d1cc1213891ac717
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-26 15:14:29 +0200

    Style markdown pages

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { CalendarDays } from 'lucide-svelte';
+    import Card from '$lib/components/Card.svelte';
+    import Hero from '$lib/components/Hero.svelte';
     import { formatDate } from '$lib/utils.js';
 
     export let data;
@@ -11,18 +12,14 @@
     <meta property="og:title" content={data.meta.title} />
 </svelte:head>
 
-<article>
-    <header class="py-12">
-        <hgroup class="flex flex-col gap-2">
-            <h1 class="text-primary text-6xl">{data.meta.title}</h1>
-            <p class="flex gap-2 text-sm text-secondary items-center">
-                <CalendarDays size="16" />
-                {formatDate(data.meta.date)}
-            </p>
-        </hgroup>
+<div class="py-12">
+    <header>
+        <Hero title={data.meta.title} subtitle={formatDate(data.meta.date)} />
     </header>
 
-    <section class="prose">
-        <svelte:component this={data.content} />
-    </section>
-</article>
+    <Card element="article">   
+        <section class="prose">
+            <svelte:component this={data.content} />
+        </section>
+    </Card>
+</div>
