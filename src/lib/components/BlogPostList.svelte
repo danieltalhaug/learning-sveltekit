adc56f6ea7a4e9de33217de8453c8b2649506524
danieltalhaug <daniel.talhaug@outlook.com>
2023-09-26 15:47:46 +0200

    Add Rows/grid toggle to list of blog posts

--- src/lib/components/BlogPostList.svelte
+++ src/lib/components/BlogPostList.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
     import type { Post } from '$lib/types';
     import BlogPostCard from '$lib/components/BlogPostCard.svelte';
+    import { Rows, LayoutGrid } from 'lucide-svelte';
+
+    type Grid = 'rows' | 'grid';
 
     export let posts: Post[];
+    let displayListAs: Grid = 'rows';
+
+    $: grid = displayListAs === 'rows' ? 'sm:grid-cols-1' : 'sm:grid-cols-2';
+    $: displayAsGrid = displayListAs === 'grid' ? 'btn-primary' : '';
+    $: displayAsRows = displayListAs === 'rows' ? 'btn-primary' : '';
+
+    function onChangeListDisplay(to: Grid) {
+        displayListAs = to;
+    }
 </script>
 
-<ul class="flex flex-col gap-8 w-full">
-    {#each posts as post}
-        <BlogPostCard {post} />
-    {/each}
-</ul>
+<article>
+    <section class="card mb-4">
+        <div class="join self-end invisible lg:visible">
+            <button class="btn join-item {displayAsRows}" on:click={() => onChangeListDisplay('rows')}>
+                <Rows />
+            </button>
+            <button class="btn join-item {displayAsGrid}" on:click={() => onChangeListDisplay('grid')}>
+                <LayoutGrid />
+            </button>
+        </div>
+    </section>
+    
+    <ul class="grid {grid} gap-8 w-full">
+        {#each posts as post}
+            <BlogPostCard {post} />
+        {/each}
+    </ul>
+</article>
